5c90ce2858a8619ab1f6bf310ef771d67fbe63c9
0xFireball <0xFireball@outlook.com>
2016-06-06 22:59:45 -0700

    basic template

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+//let msRealm = continuum.createRealm();
+
+$(document).ready(function () {
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 500);
+});
