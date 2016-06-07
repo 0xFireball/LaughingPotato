0b888aa30c48cebb35e2da65d54bfb75d5a79048
0xFireball <0xFireball@outlook.com>
2016-06-06 23:11:47 -0700

    add a bunch of descriptors

--- js/descriptors.js
+++ js/descriptors.js
@@ -1,4 +1,47 @@
-var setA = ["roasted"];
-var setB = ["green"];
-var setC = ["sliced"];
-var setD = ["avocado"];
+var setA = [
+    "roasted",
+    "microwaved",
+    "sautéed",
+    "cooked",
+    "uncooked",
+    "overcooked",
+    "half-cooked",
+    "badly-cooked",
+    "fried",
+    "deep-fried",
+    "boiled",
+    "half-boiled",
+    "smoked",
+    "burned",
+    "grilled",
+    "frozen",
+    "chilled",
+    "freeze-dried",
+    "flash-frozen",
+    "thawed"
+];
+var setB = [
+    "green",
+    "blue",
+    "evil",
+    "intriguing",
+    "malicious",
+    "happy",
+    "sad",
+    "undecided",
+    "confused"
+];
+var setC = [
+    "sliced",
+    "salted",
+    "overseasoned",
+    "unsweetened",
+    "semisweetened",
+    "charred",
+    "chopped",
+    "diced",
+    "peeled"
+];
+var setD = [
+    "avocado"
+];
