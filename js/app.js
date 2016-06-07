fb082daf4dfd003003ad0782004ea03507dfbc25
0xFireball <0xFireball@outlook.com>
2016-06-06 23:04:17 -0700

    more basic stuff

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 /*jshint esversion: 6 */
 //let msRealm = continuum.createRealm();
 
+$("#result").hide();
+
 $(document).ready(function () {
-    setTimeout(function() {
+    setTimeout(function () {
         $('body').addClass('loaded');
     }, 500);
+});
+
+function generateARandomThing() {
+    return "You are an apple.";
+}
+
+$("#btnGo").click(function () {
+    $("#result").show();
+    let randomThing = generateARandomThing();
+    $("#resVal").html(randomThing);
 });
